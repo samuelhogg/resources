51f7c395b8b4932cbe9e53bc9401ea2bdb44dcd7
samuelhogg <45640681+samuelhogg@users.noreply.github.com>
2026-06-01 19:14:28 +0100

    Add links to mkdocs resources in website.sh

---
+++ website.sh
@@ -0,0 +1,6 @@
+https://www.mkdocs.org/
+
+https://github.com/squidfunk/mkdocs-material
+
+https://dev.to/jajera/deploying-mkdocs-on-github-pages-with-devcontainers-207a
+https://github.com/jdevto/mkdocs-demo
